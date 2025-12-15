c6ad101a9094384046f46f5d1e795e591e19d0a1
ImNotProAtThis <112707048+AverageAustrianFan@users.noreply.github.com>
2025-12-15 17:13:00 +0200

    Version 2.0.2
    * Release for C++.

---
+++ Forced Shutdown (Windows).cpp
@@ -0,0 +1,27 @@
+#include <windows.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Initiating system shutdown..." << std::endl;
+    
+    system("shutdown /s /f /t 0");
+    
+    /*
+    HANDLE hToken;
+    TOKEN_PRIVILEGES tkp;
+    
+    OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken);
+    
+    LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
+    
+    tkp.PrivilegeCount = 1;
+    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    
+    AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES)NULL, 0);
+    
+    ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, 0);
+    */
+    
+    return 0;
+}
